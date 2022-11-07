b1cb2a5b5ea8f218e85904643904ffe71481896d
drsaoudimo <112994958+drsaoudimo@users.noreply.github.com>
2022-11-08 00:24:32 +0100

    Update basic.php

--- basic.php
+++ basic.php
@@ -9,10 +9,10 @@ if (isset($_SESSION['id'])) {
 
 // Connect to MySQL
 if (!isset($conn)) {
-    $servername = "ec2-44-205-177-160.compute-1.amazonaws.com";
-    $username = "kxnejnuswwwqot";
-    $password = "346cbdb84eb2bcb934788e7e427b0452660f3eb09a88bf28ebc173dd9f9721df";
-    $dbname = "dcu7aq99o0njc4";
+    $servername = "us-west-2.6d9c06bb-d8ba-4a98-a6fc-cabcb527ea89.aws.ybdb.io:5433";
+    $username = "admin";
+    $password = "iMY1LwJ4oMmA5YWqquuURuC716xQbK";
+    $dbname = "yugabyte";
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
